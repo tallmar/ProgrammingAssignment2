15d0b90191fd9afea83635a5455b623ba32922e4
mll <martellet@gmail.com>
2014-05-18 12:26:06 +0200

    test commit

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+##  test for commit Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
